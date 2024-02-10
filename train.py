1a669419eb3bf839117d346712756e4988afc0ae
nastyabekesheva <nastya.bekesheva@gmail.com>
2024-02-10 23:08:18 +0200

    add training

---
+++ train.py
@@ -0,0 +1,21 @@
+import sys
+import os
+import utils.preprocessing
+from ultralytics import YOLO
+import wandb
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python train.py <path_to_file>")
+        return
+    dataset_path = sys.argv[1]
+
+    print("Preprocessing dataset...")
+    utils.preprocessing(dataset_path)
+    
+    model = YOLO('yolov8l.pt')
+    wandb.login(key='da27b138f0f392e3f931cf71acdab08543ac649c')
+    results = model.train(data='config.yaml', epochs = 1)
+
+if __name__ == "__main__":
+    main()
