f11e34884bab27c2a0e3a5d6f1a74629f4541f8d
nastyabekesheva <nastya.bekesheva@gmail.com>
2024-02-10 23:20:37 +0200

    fixx args for running

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -6,72 +6,9 @@ from tqdm import tqdm
 import pydicom
 from PIL import Image
 import shutil
-
-'''try:
-    shutil.rmtree('/kaggle/working/')
-except:
-    pass'''
-
-
-# download dataset
-dataset = 'https://www.kaggle.com/'
-od.download(dataset)
+import sys
 
 IMG_SIZE = 1024
-
-# paths
-CSV_FILE = '../input/rsna-pneumonia-detection-challenge/stage_2_train_labels.csv'
-TRAIN_SRC_DIR = '../input/rsna-pneumonia-detection-challenge/stage_2_train_images/'
-TEST_SRC_DIR = '../input/rsna-pneumonia-detection-challenge/stage_2_test_images/'
-DATASET_DIR = './dataset/'
-TEST_IMG_DIR = 'test_images/'
-
-os.mkdir(DATASET_DIR)
-os.mkdir(DATASET_DIR + 'images/')
-os.mkdir(DATASET_DIR + 'images/train/')
-os.mkdir(DATASET_DIR + 'images/val/')
-os.mkdir(DATASET_DIR + 'images/test/')
-os.mkdir(DATASET_DIR + 'labels/')
-os.mkdir(DATASET_DIR + 'labels/train/')
-os.mkdir(DATASET_DIR + 'labels/val/')
-os.mkdir(DATASET_DIR + 'labels/test/')
-os.mkdir(TEST_IMG_DIR)
-
-# read dataset
-annotations = pd.read_csv(CSV_FILE)
-
-# cleaning
-positive_annotations = annotations[annotations.Target == 1]
-negative_annotations = annotations[annotations.Target == 0]
-negative_sample = negative_annotations.sample(600)
-negative_sample['patientId'].shape[0]
-
-annotations = pd.concat([positive_annotations, negative_sample])
-patient_id_series = annotations.patientId.drop_duplicates()
-
-# train test split
-print('Number of images:', patient_id_series.size)
-train_series, val_series = train_test_split(patient_id_series, test_size=0.1, random_state=42)
-print('Train set number:', len(train_series))
-print('Validation set number:', len(val_series))
-
-# read images
-for patient_id in tqdm(train_series):
-    src_path = TRAIN_SRC_DIR + patient_id + '.dcm'
-    dcm_data = pydicom.dcmread(src_path)
-    image_array = dcm_data.pixel_array
-    image = Image.fromarray(image_array)
-    image.save(DATASET_DIR + 'images/train/' + patient_id + '.jpg')
-print('Images moved to train folder:', len(os.listdir(DATASET_DIR + 'images/train/')))
-    
-for patient_id in tqdm(val_series):
-    src_path = TRAIN_SRC_DIR + patient_id + '.dcm'
-    dcm_data = pydicom.dcmread(src_path)
-    image_array = dcm_data.pixel_array
-    image = Image.fromarray(image_array)
-    image.save(DATASET_DIR + 'images/val/' + patient_id + '.jpg')
-print('Images moved to val folder:', len(os.listdir(DATASET_DIR + 'images/val/')))
-
 # prepare coordinates for training
 def translate_bbox(bbox):
     top_left_x = bbox[0]
@@ -111,19 +48,84 @@ def save_label(label_dir, patient_id, bbox):
     f.write(line)
     f.close()
 
-LABELS_DIR = "./labels_temp/"
-os.mkdir(LABELS_DIR)
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python preprocessing.py <path_to_file>")
+        return
+    dataset_path = sys.argv[1]
 
-for row in annotations.values:
-    if pd.notna(row[1:5]).all():
-        save_label(LABELS_DIR, row[0], row[1:5])
-    
-for patient_id in train_series:
-    if os.path.isfile(LABELS_DIR + patient_id + '.txt'):
-        shutil.copy(LABELS_DIR + patient_id + '.txt', DATASET_DIR + 'labels/train/')
-    
-for patient_id in val_series:
-    if os.path.isfile(LABELS_DIR + patient_id + '.txt'):
-        shutil.copy(LABELS_DIR + patient_id + '.txt', DATASET_DIR + 'labels/val/')
-    
-shutil.rmtree(LABELS_DIR)
+    # paths
+    CSV_FILE = dataset_path + 'stage_2_train_labels.csv'
+    TRAIN_SRC_DIR = dataset_path + 'stage_2_train_images/'
+    TEST_SRC_DIR = dataset_path + 'stage_2_test_images/'
+    DATASET_DIR = './dataset/'
+    TEST_IMG_DIR = 'test_images/'
+
+    os.mkdir(DATASET_DIR)
+    os.mkdir(DATASET_DIR + 'images/')
+    os.mkdir(DATASET_DIR + 'images/train/')
+    os.mkdir(DATASET_DIR + 'images/val/')
+    os.mkdir(DATASET_DIR + 'images/test/')
+    os.mkdir(DATASET_DIR + 'labels/')
+    os.mkdir(DATASET_DIR + 'labels/train/')
+    os.mkdir(DATASET_DIR + 'labels/val/')
+    os.mkdir(DATASET_DIR + 'labels/test/')
+    os.mkdir(TEST_IMG_DIR)
+
+    # read dataset
+    annotations = pd.read_csv(CSV_FILE)
+
+    # cleaning
+    positive_annotations = annotations[annotations.Target == 1]
+    negative_annotations = annotations[annotations.Target == 0]
+    negative_sample = negative_annotations.sample(600)
+    negative_sample['patientId'].shape[0]
+
+    annotations = pd.concat([positive_annotations, negative_sample])
+    patient_id_series = annotations.patientId.drop_duplicates()
+
+    # train test split
+    print('Number of images:', patient_id_series.size)
+    train_series, val_series = train_test_split(patient_id_series, test_size=0.1, random_state=42)
+    print('Train set number:', len(train_series))
+    print('Validation set number:', len(val_series))
+
+    # read images
+    for patient_id in tqdm(train_series):
+        src_path = TRAIN_SRC_DIR + patient_id + '.dcm'
+        dcm_data = pydicom.dcmread(src_path)
+        image_array = dcm_data.pixel_array
+        image = Image.fromarray(image_array)
+        image.save(DATASET_DIR + 'images/train/' + patient_id + '.jpg')
+    print('Images moved to train folder:', len(os.listdir(DATASET_DIR + 'images/train/')))
+        
+    for patient_id in tqdm(val_series):
+        src_path = TRAIN_SRC_DIR + patient_id + '.dcm'
+        dcm_data = pydicom.dcmread(src_path)
+        image_array = dcm_data.pixel_array
+        image = Image.fromarray(image_array)
+        image.save(DATASET_DIR + 'images/val/' + patient_id + '.jpg')
+    print('Images moved to val folder:', len(os.listdir(DATASET_DIR + 'images/val/')))
+
+
+
+    LABELS_DIR = "./labels_temp/"
+    os.mkdir(LABELS_DIR)
+
+    for row in annotations.values:
+        if pd.notna(row[1:5]).all():
+            save_label(LABELS_DIR, row[0], row[1:5])
+        
+    for patient_id in train_series:
+        if os.path.isfile(LABELS_DIR + patient_id + '.txt'):
+            shutil.copy(LABELS_DIR + patient_id + '.txt', DATASET_DIR + 'labels/train/')
+        
+    for patient_id in val_series:
+        if os.path.isfile(LABELS_DIR + patient_id + '.txt'):
+            shutil.copy(LABELS_DIR + patient_id + '.txt', DATASET_DIR + 'labels/val/')
+        
+    shutil.rmtree(LABELS_DIR)
+
+if __name__ == "__main__":
+    main()
+
