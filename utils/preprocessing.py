37bf4a0c6ed9eedbd1fd3e5b0d9694f9f62a25a7
Borys Kovba <borys.kovba@gmail.com>
2024-02-11 14:18:55 +0200

    docker

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -6,46 +6,26 @@ import pydicom
 from PIL import Image
 import shutil
 import sys
+from utils.coordinates import translate_bbox, revert_bbox
 
 IMG_SIZE = 1024
-# prepare coordinates for training
-def translate_bbox(bbox):
-    top_left_x = bbox[0]
-    top_left_y = bbox[1]
-    absolute_w = bbox[2]
-    absolute_h = bbox[3]
 
-    relative_w = absolute_w / IMG_SIZE
-    relative_h = absolute_h / IMG_SIZE
-    
-    relative_x = top_left_x / IMG_SIZE + relative_w / 2
-    relative_y = top_left_y / IMG_SIZE + relative_h / 2
-    
-    return relative_x, relative_y, relative_w, relative_h
-    
-def revert_bbox(rx, ry, rw, rh):
-    x = (rx-rw/2)*IMG_SIZE
-    y = (ry-rh/2)*IMG_SIZE
-    w = rw*IMG_SIZE
-    h = rh*IMG_SIZE
-    
-    return x, y, w, h
-    
-    
+
 def save_label(label_dir, patient_id, bbox):
     label_fp = os.path.join(label_dir, patient_id + '.txt')
-    
+
     f = open(label_fp, "a")
     if (bbox == 'nan').all():
         f.close()
         return
-    
+
     x, y, w, h = translate_bbox(bbox)
-    
+
     line = f"0 {x} {y} {w} {h}\n"
-    
+
     f.write(line)
     f.close()
+
 
 def main(path):
     if len(sys.argv) < 2:
@@ -97,7 +77,7 @@ def main(path):
         image = Image.fromarray(image_array)
         image.save(DATASET_DIR + 'images/train/' + patient_id + '.jpg')
     print('Images moved to train folder:', len(os.listdir(DATASET_DIR + 'images/train/')))
-        
+
     for patient_id in tqdm(val_series):
         src_path = TRAIN_SRC_DIR + patient_id + '.dcm'
         dcm_data = pydicom.dcmread(src_path)
@@ -106,25 +86,23 @@ def main(path):
         image.save(DATASET_DIR + 'images/val/' + patient_id + '.jpg')
     print('Images moved to val folder:', len(os.listdir(DATASET_DIR + 'images/val/')))
 
-
-
     LABELS_DIR = "./labels_temp/"
     os.mkdir(LABELS_DIR)
 
     for row in annotations.values:
         if pd.notna(row[1:5]).all():
             save_label(LABELS_DIR, row[0], row[1:5])
-        
+
     for patient_id in train_series:
         if os.path.isfile(LABELS_DIR + patient_id + '.txt'):
             shutil.copy(LABELS_DIR + patient_id + '.txt', DATASET_DIR + 'labels/train/')
-        
+
     for patient_id in val_series:
         if os.path.isfile(LABELS_DIR + patient_id + '.txt'):
             shutil.copy(LABELS_DIR + patient_id + '.txt', DATASET_DIR + 'labels/val/')
-        
+
     shutil.rmtree(LABELS_DIR)
+
 
 if __name__ == "__main__":
     main()
-
